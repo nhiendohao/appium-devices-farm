17de3df254f8a966ce9acf1c4e3f69a639a36790
Nguyen Cao Cuong <cuongnguyen4285@gmail.com>
2023-06-19 15:53:19 +0700

    Add get mobile platform method

--- src/main/java/automation/example/demo/drivermanager/DriverManager.java
+++ src/main/java/automation/example/demo/drivermanager/DriverManager.java
@@ -1,11 +1,18 @@
 package automation.example.demo.drivermanager;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.NoSuchDriverException;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import net.bytebuddy.implementation.bytecode.Throw;
 
 import automation.example.demo.drivermanager.appiumdriver.AppiumManager;
 import automation.example.demo.drivermanager.appiumdriver.DesiredCapabilityBuilder;
 import automation.example.demo.drivermanager.webdriver.WebDriverFactory;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
 
 public class DriverManager {
 
@@ -14,8 +21,25 @@ public class DriverManager {
         return WebDriverFactory.startWebDriver(browser);
     }
 
-    public static AppiumDriver getMobileDriver(String deviceUdid) {;
+    public static AppiumDriver getMobileDriver(String deviceUdid) {
+        ;
         final DesiredCapabilityBuilder builder = new DesiredCapabilityBuilder();
         return AppiumManager.startAppiumDriver(builder.buildDesiredCapabilities(deviceUdid));
     }
+
+    public static Platform getMobilePlatform(WebDriver driver) {
+        try {
+            Platform platform = ((RemoteWebDriver) driver).getCapabilities().getPlatformName();
+            if (Platform.ANDROID.equals(platform)) {
+                return Platform.ANDROID;
+            } else if (Platform.IOS.equals(platform)){
+                return Platform.IOS;
+            } else {
+                return Platform.MAC;
+            }
+        } catch (Exception ex) {
+            throw new NoSuchDriverException("Unknown driver");
+        }
+    }
+
 }
